47bba22b94f0686a82479babf8ec648be46df0c0
Helder <helder@lamoulade.com>
2016-02-24 17:58:35 +0000

    run the example on yield delegation; not quite sure how this works; confusing

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var koa = require('koa');
 var route = require('koa-route');
 var app = koa();
 
+function *index() {
+	this.body = "<h1>Hello! This is my home page!</h1>";
+}
+
+function *about() {
+	this.body = "<h2>My name is Adam and I like Javacript</h2>";
+}
+
 app.use(route.get('/', index));
 app.use(route.get('/about', about));
 
